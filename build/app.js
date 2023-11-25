294b9c5482843c087817a90945fe9a763a329161
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-11-25 14:03:35 +0530

    chore: updated cors

--- build/app.js
+++ build/app.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
-require("dotenv").config();
+require('dotenv').config();
 const express_1 = __importDefault(require("express"));
 exports.app = (0, express_1.default)();
 const cors_1 = __importDefault(require("cors"));
@@ -18,12 +18,12 @@ const analytics_route_1 = __importDefault(require("./routes/analytics.route"));
 const layout_route_1 = __importDefault(require("./routes/layout.route"));
 const express_rate_limit_1 = require("express-rate-limit");
 // body parser
-exports.app.use(express_1.default.json({ limit: "50mb" }));
+exports.app.use(express_1.default.json({ limit: '50mb' }));
 // cookie parser
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: ["https://e-learning-client-nine.vercel.app"],
+    origin: ['*'],
     credentials: true,
 }));
 // api requests limit
@@ -34,16 +34,16 @@ const limiter = (0, express_rate_limit_1.rateLimit)({
     legacyHeaders: false,
 });
 // routes
-exports.app.use("/api/v1", user_route_1.default, order_route_1.default, course_route_1.default, notification_route_1.default, analytics_route_1.default, layout_route_1.default);
+exports.app.use('/api/v1', user_route_1.default, order_route_1.default, course_route_1.default, notification_route_1.default, analytics_route_1.default, layout_route_1.default);
 // testing api
-exports.app.get("/test", (req, res, next) => {
+exports.app.get('/test', (req, res, next) => {
     res.status(200).json({
         succcess: true,
-        message: "API is working",
+        message: 'API is working',
     });
 });
 // unknown route
-exports.app.all("*", (req, res, next) => {
+exports.app.all('*', (req, res, next) => {
     const err = new Error(`Route ${req.originalUrl} not found`);
     err.statusCode = 404;
     next(err);
